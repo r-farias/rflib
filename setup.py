b85c2858b1fc01bc334cca365e7c0e723c3b5bd0
Roberto Farias <rfarias@comparaonline.com>
2020-01-24 12:18:48 -0300

    setup.py updated

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='rflib',
-    version='0.1',
+    version='0.1.1',
     description='This is a test for a tutorial on how to create a python library',  # noqa
     url='git@github.com:r-farias/rflib.git',
-    download_url='https://github.com/r-farias/rflib/archive/v0.1.tar.gz',  # noqa
+    download_url='https://github.com/r-farias/rflib/archive/v0.1.1.tar.gz',  # noqa
     author='Roberto Farias',
     author_email='rfarias@comparaonline.com',
     license='MIT',
