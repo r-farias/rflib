6e1e3c7a7f7f70a8762c1f304c9c2e610fd314c0
Roberto Farias <rfarias@comparaonline.com>
2020-01-24 11:45:37 -0300

    licence and setup added

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='rflib',
+    version='0.1',
+    description='This is a test for a tutorial on how to create a python library',  # noqa
+    url='git@github.com:r-farias/rflib.git',
+    author='Roberto Farias',
+    author_email='rfarias@comparaonline.com',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+    ]
+)
