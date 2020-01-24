f043c5ce39c93278aac0a90c395efce640fe47b3
Roberto Farias <rfarias@comparaonline.com>
2020-01-24 11:52:50 -0300

    download_url added to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     version='0.1',
     description='This is a test for a tutorial on how to create a python library',  # noqa
     url='git@github.com:r-farias/rflib.git',
+    download_url='https://github.com/r-farias/rflib/archive/v0.1.tar.gz',  # noqa
     author='Roberto Farias',
     author_email='rfarias@comparaonline.com',
     license='MIT',
